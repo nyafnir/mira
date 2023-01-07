e72a1e401bbcb1e5ec5f8440da3c8964fce5b0b6
nyafnir <nyafirawr@gmail.com>
2023-01-07 19:18:48 +0700

    Изменил логику сервиса кэша на подключение вручную

--- src/services/cache/cache.service.ts
+++ src/services/cache/cache.service.ts
@@ -9,12 +9,23 @@ export class CacheService {
   private readonly config: CacheConfigInterface =
     configService.validateConfig(CacheConfigSchema)
 
-  public readonly redis: Redis
+  public connect(dbIndex = 0) {
+    console.debug(
+      this.constructor.name,
+      `Подключаюсь к базе кэша №${dbIndex} ...`,
+    )
 
-  constructor() {
-    this.redis = new Redis({
+    const instance = new Redis({
       host: this.config.REDIS_HOST,
       port: this.config.REDIS_PORT,
+      db: dbIndex,
     })
+
+    console.debug(
+      this.constructor.name,
+      `Соединение с базой кэша №${dbIndex} установлено! ✅`,
+    )
+
+    return instance
   }
 }
--- src/services/cache/index.ts
+++ src/services/cache/index.ts
@@ -3,6 +3,5 @@ import Redis from 'ioredis'
 import { CacheService } from './cache.service'
 
 const cacheService = new CacheService()
-const redis = cacheService.redis
 
-export { redis, Redis }
+export { cacheService, CacheService, Redis }
